0bcd7aa245dc0751f08338c8f34bbf114538f76d
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-06-01 00:25:03 -0300

    add: lesson 10 js

---
+++ build/lesson10.js
@@ -0,0 +1,10 @@
+"use strict";
+//NULL - UNDEFINED - UNKNOWN
+let name1;
+name1 = null;
+console.log(name1); // null é como se fosse um tipo de dado (tipo nulo)
+let name2;
+console.log(name2); // indefined é quando o tipo não foi definido na criação de uma variavel/constante.(tipo ANY)
+let name3; // unknown pode receber qualquer tipo.
+let aux = name3;
+console.group(name3); //unknown só pode ser usado em dados do tipo unknown ou do tipo any apenas.
