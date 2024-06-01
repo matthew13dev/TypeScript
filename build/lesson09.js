64caeab1ba4063f53af4e48aff88c0c4f4f7426d
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-06-01 00:01:18 -0300

    add: lesson 09

--- build/lesson09.js
+++ build/lesson09.js
@@ -1,2 +1,22 @@
 "use strict";
-//ENUMERADOS
+//ENUM
+var weekdays;
+(function (weekdays) {
+    weekdays[weekdays["sunday"] = 0] = "sunday";
+    weekdays[weekdays["monday"] = 1] = "monday";
+    weekdays[weekdays["tuesday"] = 2] = "tuesday";
+    weekdays[weekdays["wednesday"] = 3] = "wednesday";
+    weekdays[weekdays["thursday"] = 4] = "thursday";
+    weekdays[weekdays["friday"] = 5] = "friday";
+    weekdays[weekdays["saturnday"] = 6] = "saturnday";
+})(weekdays || (weekdays = {}));
+let date = new Date();
+console.log(weekdays[date.getDay()]);
+var userType;
+(function (userType) {
+    userType[userType["USER"] = 0] = "USER";
+    userType[userType["ADMIN"] = 1] = "ADMIN";
+    userType[userType["SUPER"] = 2] = "SUPER";
+})(userType || (userType = {}));
+const userJob = userType.SUPER;
+console.log(userJob);
--- src/lesson09.ts
+++ src/lesson09.ts
@@ -1,1 +1,24 @@
-//ENUMERADOS
+//ENUM
+
+enum weekdays {
+    sunday =  0,
+    monday = 1,
+    tuesday = 2,
+    wednesday = 3,
+    thursday = 4,
+    friday = 5,
+    saturnday = 6,
+}
+
+let date = new Date();
+
+console.log(weekdays[date.getDay()])
+
+enum userType {
+    USER = 0,
+    ADMIN = 1,
+    SUPER = 2
+}
+
+const userJob: userType = userType.SUPER;
+console.log(userJob);
