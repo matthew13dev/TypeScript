431c412f0837e2c921d776f03efe03275fa7d698
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-06-10 02:35:51 -0300

    change: lesson 17

--- build/lesson16.js
+++ build/lesson16.js
@@ -1,16 +1,44 @@
 "use strict";
-// lesson 16 - POO
+// lesson 16 - POO #1
 class Computer {
-    name = "";
-    processador = "";
-    placa = "";
-    ram = 0;
+    name;
+    processador;
+    ram;
     on = false;
-    constructor(name) {
+    //metodo construtor
+    constructor(name, processador, ram) {
         this.name = name;
+        this.processador = processador;
+        this.ram = ram;
+        console.log("novo computador criado: " + this.name);
+    }
+    print() {
+        console.log("---------------------------------");
+        console.log(`name: ${this.name}`);
+        console.log(`processador: ${this.processador}`);
+        console.log(`ram: ${this.ram}GB`);
+    }
+    turnON() {
+        if (this.on === false) {
+            this.on = true;
+            console.log("computer TURN-ON");
+        }
+        else {
+            this.on = false;
+            console.log("computer TURN-OFF");
+        }
     }
 }
-const computer1 = new Computer("gamer");
-const computer2 = new Computer("office");
-console.log(computer1);
-console.log(computer2);
+// instanciar e construtor
+const computer1 = new Computer("gamer", "AMD Ryzen 5 4600G", 16);
+const computer2 = new Computer("office", "AMD Ryzen 7 5700X3D", 32);
+computer1.print();
+computer2.print();
+console.log("-----------------------");
+console.log("-----------------------");
+computer1.turnON();
+computer2.turnON();
+computer1.turnON();
+computer2.turnON();
+computer1.turnON();
+computer2.turnON();
--- src/lesson16.ts
+++ src/lesson16.ts
@@ -1,21 +1,54 @@
-// lesson 16 - POO #1
+// lesson 16, 17 - POO #1
 
 
 class Computer {
-    name:string = "";
-    processador:string = "";
-    placa:string = "";
-    ram:number = 0;
-    on:boolean = false;
+    private name:string;
+    private processador:string;
+    private ram:number;
+    private on:boolean = false;
 
-    constructor(name:string){
+    //metodo construtor
+    constructor(name:string, processador:string, ram:number){
         this.name = name;
+        this.processador = processador;
+        this.ram = ram;
+        console.log("novo computador criado: " + this.name)
+    }
+
+    print(){
+        console.log("---------------------------------")
+        console.log(`name: ${this.name}`);
+        console.log(`processador: ${this.processador}`);
+        console.log(`ram: ${this.ram}GB`);
+    }
+
+    turnON(){
+        if(this.on === false){
+            this.on = true;
+            console.log("computer TURN-ON");
+        } else {
+            this.on = false;
+            console.log("computer TURN-OFF");
+        }
     }
 
 }
 
-const computer1 = new Computer("gamer");
-const computer2 = new Computer("office");
+// instanciar e construtor
+const computer1 = new Computer("gamer", "AMD Ryzen 5 4600G", 16);
+const computer2 = new Computer("office", "AMD Ryzen 7 5700X3D", 32);
 
-console.log(computer1);
-console.log(computer2);
+computer1.print();
+computer2.print();
+
+console.log("-----------------------")
+console.log("-----------------------")
+
+computer1.turnON()
+computer2.turnON()
+
+computer1.turnON()
+computer2.turnON()
+
+computer1.turnON()
+computer2.turnON()
